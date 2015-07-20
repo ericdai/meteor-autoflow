0325bc0b88765d71021568c6ae62d1fdbc36247d
Dave Allen <david.allen@meliortechnology.com>
2015-07-20 09:01:28 -0700

    Updating version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'fullflavedave:autoflow',
-    version: '0.0.7',
+    version: '0.0.8',
     summary: 'Produce multi-page forms and page flows entirely from JSON',
     git: '',
     // By default, Meteor will default to using README.md for documentation.
